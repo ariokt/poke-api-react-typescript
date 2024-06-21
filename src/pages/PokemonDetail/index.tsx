ed6bdf6bb395abef2cdeebbaec661ac2697084f0
ariokt <ariowta@gmail.com>
2024-06-21 10:58:40 +0700

    fix: revise page pokemon detail table

--- src/pages/PokemonDetail/index.tsx
+++ src/pages/PokemonDetail/index.tsx
@@ -35,18 +35,20 @@ function PokemonDetail() {
             <div className='flex flex-col translate-y-[-1rem]'>
               <h3 className='text-2xl mx-auto mb-2 font-bold'>{detailPokemon.species.name.charAt(0).toUpperCase() + detailPokemon.species.name.slice(1)}</h3>
               <div className='flex gap-2 justify-center'>{detailPokemon.types.map((typeInfo: any) => <TagPokemonType key={typeInfo.type.name} pokemonType={typeInfo.type.name} />)}</div>
-              <table className='mt-8 mx-auto w-fit table-auto bg-white border border-gray-300'>
-                <thead>
-                  <tr>
-                    {detailPokemon.stats.map((stat: any) => <th key={stat.stat.name} className='px-4 py-2 border-b-2 border-gray-300 text-sm'>{stat.stat.name}</th>)}
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    {detailPokemon.stats.map((stat: any) => <td key={stat.stat.name} className='px-4 py-2 border-b-2 border-gray-300 text-sm'>{stat.base_stat}</td>)}
-                  </tr>
-                </tbody>
-              </table>
+              <div className='overflow-x-scroll'>
+                <table className='mt-8 mx-auto w-fit table-auto bg-white border border-gray-300 overflow-x-scroll'>
+                  <thead>
+                    <tr>
+                      {detailPokemon.stats.map((stat: any) => <th key={stat.stat.name} className='px-4 py-2 border-2 border-gray-300 text-sm md:text-base'>{stat.stat.name}</th>)}
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      {detailPokemon.stats.map((stat: any) => <td key={stat.stat.name} className='px-4 py-2 border-2 border-gray-300 text-sm md:text-base'>{stat.base_stat}</td>)}
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
           :
